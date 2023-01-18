2b9ddcd16c915b80619fdd2ffff80eac21cc87ff
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-01-18 14:34:05 -0300

    Ajuste

--- src/components/Favoritos/index.jsx
+++ src/components/Favoritos/index.jsx
@@ -50,100 +50,102 @@ function index() {
   }
 
   return (
-    <TableContainer sx={tablestyles} component={Box}>
+    <>
       {favoritos.length > 0 ? (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <StyledTableCell></StyledTableCell>
-              <StyledTableCell>NOME</StyledTableCell>
-              <StyledTableCell>ALBÚM</StyledTableCell>
-              <StyledTableCell>CANTOR</StyledTableCell>
-              <StyledTableCell>DURAÇÃO</StyledTableCell>
-              <StyledTableCell></StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {favoritos.map((musica) => {
-              let audio = new Audio(`${musica.audio}`);
+        <TableContainer sx={tablestyles} component={Box}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell></StyledTableCell>
+                <StyledTableCell>NOME</StyledTableCell>
+                <StyledTableCell>ALBÚM</StyledTableCell>
+                <StyledTableCell>CANTOR</StyledTableCell>
+                <StyledTableCell>DURAÇÃO</StyledTableCell>
+                <StyledTableCell></StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {favoritos.map((musica) => {
+                let audio = new Audio(`${musica.audio}`);
 
-              const start = () => {
-                audio.play();
-              };
+                const start = () => {
+                  audio.play();
+                };
 
-              const pause = () => {
-                audio.pause();
-              };
+                const pause = () => {
+                  audio.pause();
+                };
 
-              let minutoDuracao = Math.trunc(musica.duracao / 60);
-              let segundoDuracao = musica.duracao % 60;
+                let minutoDuracao = Math.trunc(musica.duracao / 60);
+                let segundoDuracao = musica.duracao % 60;
 
-              const icons = (
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "space-around",
-                  }}
-                >
-                  <Tooltip title="Música completa">
-                    <MusicNoteIcon sx={icon}>
-                      <a
-                        style={{
-                          textDecoration: "none",
-                          color: "white",
-                        }}
-                        href={musica.link}
-                        target="_blank"
-                      ></a>
-                    </MusicNoteIcon>
-                  </Tooltip>
-                  <Tooltip title="Prévia">
-                    <Play start={start} pause={pause} icon={icon} />
-                  </Tooltip>
-                  <Tooltip title="Remover">
-                    <DeleteIcon
-                      style={{ ...icon, color: pink[600] }}
-                      onClick={() => {
-                        removeFromFav(musica.id);
-                      }}
-                    />
-                  </Tooltip>
-                </div>
-              );
-              return (
-                <TableRow
-                  key={musica.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <StyledTableCell>
-                    <Tooltip title={musica.titulo}>
-                      <Avatar src={musica.foto} variant="rounded" />
+                const icons = (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-around",
+                    }}
+                  >
+                    <Tooltip title="Música completa">
+                      <MusicNoteIcon sx={icon}>
+                        <a
+                          style={{
+                            textDecoration: "none",
+                            color: "white",
+                          }}
+                          href={musica.link}
+                          target="_blank"
+                        ></a>
+                      </MusicNoteIcon>
                     </Tooltip>
-                  </StyledTableCell>
-                  <StyledTableCell sx={td}>
-                    <p style={tp}>{musica.titulo}</p>
-                  </StyledTableCell>
-                  <StyledTableCell>{musica.autor}</StyledTableCell>
-                  <StyledTableCell sx={td}>
-                    <p style={tp}>{musica.titulo}</p>
-                  </StyledTableCell>
-                  <StyledTableCell>
-                    {minutoDuracao}:
-                    {segundoDuracao < 10
-                      ? "0" + segundoDuracao
-                      : segundoDuracao}
-                  </StyledTableCell>
-                  <StyledTableCell>{icons}</StyledTableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+                    <Tooltip title="Prévia">
+                      <Play start={start} pause={pause} icon={icon} />
+                    </Tooltip>
+                    <Tooltip title="Remover">
+                      <DeleteIcon
+                        style={{ ...icon, color: pink[600] }}
+                        onClick={() => {
+                          removeFromFav(musica.id);
+                        }}
+                      />
+                    </Tooltip>
+                  </div>
+                );
+                return (
+                  <TableRow
+                    key={musica.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <StyledTableCell>
+                      <Tooltip title={musica.titulo}>
+                        <Avatar src={musica.foto} variant="rounded" />
+                      </Tooltip>
+                    </StyledTableCell>
+                    <StyledTableCell sx={td}>
+                      <p style={tp}>{musica.titulo}</p>
+                    </StyledTableCell>
+                    <StyledTableCell>{musica.autor}</StyledTableCell>
+                    <StyledTableCell sx={td}>
+                      <p style={tp}>{musica.titulo}</p>
+                    </StyledTableCell>
+                    <StyledTableCell>
+                      {minutoDuracao}:
+                      {segundoDuracao < 10
+                        ? "0" + segundoDuracao
+                        : segundoDuracao}
+                    </StyledTableCell>
+                    <StyledTableCell>{icons}</StyledTableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       ) : (
         <Empty>Não tem nada aqui... Adicione músicas na aba 'Descubra'!</Empty>
       )}
-    </TableContainer>
+    </>
   );
 }
 
